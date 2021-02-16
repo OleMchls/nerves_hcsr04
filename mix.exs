4a69c40b86ad715ac5637e14b9808cf95dd9dc68
Ole Michaelis <Ole.Michaelis@googlemail.com>
2021-02-16 17:54:57 +0100

    prepare release to hex

--- mix.exs
+++ mix.exs
@@ -6,22 +6,42 @@ defmodule Hcsr04.MixProject do
       app: :hcsr04,
       version: "0.1.0",
       elixir: "~> 1.11",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Nerves_Hcsr04",
+      source_url: "https://github.com/elixir-ecto/postgrex"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:circuits_gpio, "~> 0.4"}
+      {:circuits_gpio, "~> 0.4"},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Interface with the HC-SR04 ultrasonic distance sensor."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "hcsr04",
+      # These are the default files included in the package
+      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/OleMchls/nerves_hcsr04"}
     ]
   end
 end
